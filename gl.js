ae4182864585c7d78db9393a431e597391ad708a
bjlofton <bigbbree@aol.com>
2018-01-05 12:46:04 -0500

    Add files via upload

--- gl.js
+++ gl.js
@@ -2,8 +2,10 @@ function myFunction(x) {
     x.classList.toggle("change");
     $('nav li').slideToggle();
 }
+$(document).ready(function(){
 $("#display").html(
        $(window).width());
+});
 $(window).resize(function() {
 	location.reload();
         $("#display").html(
